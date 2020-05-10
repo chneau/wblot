e8de48b9c6551f9fa8cd78ca9fba872d15f62da8
chneau <charles63500@gmail.com>
2020-05-10 01:26:49 +0100

    selection

--- public/index.js
+++ public/index.js
@@ -1,25 +1,68 @@
-function init() {
+function init(canvas, image) {
+    canvas.setWidth(document.body.offsetWidth);
+    canvas.setHeight(document.querySelector(".canvas-container").offsetHeight);
     window.ondragover = event => event.preventDefault();
-    const image = new Image();
-    const canvas = document.querySelector("#canvas");
-    canvas.width = canvas.scrollWidth;
-    canvas.height = canvas.scrollHeight;
-    const ctx = canvas.getContext("2d");
+    window.onresize = () => {
+        canvas.setWidth(document.body.offsetWidth);
+        canvas.setHeight(document.querySelector(".canvas-container").offsetHeight);
+        canvas.setBackgroundImage(new fabric.Image(image), canvas.renderAll.bind(canvas), { scaleX: canvas.width / image.width, scaleY: canvas.height / image.height });
+    }
     window.ondrop = async event => {
         event.preventDefault();
         image.src = (window.webkitURL ? webkitURL : URL).createObjectURL(event.dataTransfer.files[0]);
         await new Promise(resolve => image.onload = resolve);
-        canvas.width = image.width;
-        canvas.height = image.height;
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, canvas.width, canvas.height);
+        canvas.setBackgroundImage(new fabric.Image(image), canvas.renderAll.bind(canvas), { scaleX: canvas.width / image.width, scaleY: canvas.height / image.height });
     };
-    canvas.onclick = event => {
-        var x = (event.offsetX * (canvas.width / canvas.scrollWidth) + 0.5) | 0;
-        var y = (event.offsetY * (canvas.height / canvas.scrollHeight) + 0.5) | 0;
-        console.log(x, y);
-    };
+}
+var selection;
+function handleSelection(canvas) {
+    var isDown, origX, origY;
+    canvas.on('mouse:down', event => {
+        isDown = true;
+        var pointer = canvas.getPointer(event.e);
+        origX = pointer.x;
+        origY = pointer.y;
+        var pointer = canvas.getPointer(event.e);
+        if (selection != null) {
+            canvas.remove(selection);
+            selection = null;
+        }
+        selection = new fabric.Rect({
+            left: origX,
+            top: origY,
+            originX: 'left',
+            originY: 'top',
+            width: pointer.x - origX,
+            height: pointer.y - origY,
+            angle: 0,
+            fill: 'rgba(255,0,0,0.1)',
+            transparentCorners: false,
+            hasRotatingPoint: false,
+            cornerSize: 5
+        });
+        canvas.add(selection);
+    });
+    canvas.on('mouse:move', event => {
+        if (!isDown) return;
+        var pointer = canvas.getPointer(event.e);
+        if (origX > pointer.x) {
+            selection.set({ left: Math.abs(pointer.x) });
+        }
+        if (origY > pointer.y) {
+            selection.set({ top: Math.abs(pointer.y) });
+        }
+        selection.set({ width: Math.abs(origX - pointer.x) });
+        selection.set({ height: Math.abs(origY - pointer.y) });
+        canvas.renderAll();
+    });
+    canvas.on('mouse:up', () => {
+        isDown = false;
+    });
 }
 
 window.onload = () => {
-    init();
+    const canvas = new fabric.Canvas("canvas", { backgroundColor: null });
+    const image = new Image();
+    init(canvas, image);
+    handleSelection(canvas);
 };
