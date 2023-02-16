ae6cecba0ac2cbef2f5d7dc8c423f0f663378b28
chneau <charles63500@gmail.com>
2023-02-16 23:57:34 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
-
 export const App = () => {
-  const [count, setCount] = useState(0);
-  return <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>;
+  return (
+    <div>
+      <div>Hello World</div>
+      <div>Hello World</div>
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("No root element found");
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
