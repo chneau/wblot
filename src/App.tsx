a2466b438ebd9f712b10ffc26e0c73e3c75d1ea7
chneau <charles63500@gmail.com>
2023-02-17 09:27:45 +0000

    using Konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import Konva from "konva";
+import { useState } from "react";
+import { Layer, Rect, Stage, Text } from "react-konva";
+
+const ColoredRect = () => {
+  const [color, setColor] = useState(Konva.Util.getRandomColor());
+  const handleClick = () => setColor(Konva.Util.getRandomColor());
+  return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={handleClick} />;
+};
+
 export const App = () => {
   return (
-    <div>
-      <div>Hello World</div>
-      <div>Hello World</div>
-    </div>
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try click on rect" />
+        <ColoredRect />
+      </Layer>
+    </Stage>
   );
 };
