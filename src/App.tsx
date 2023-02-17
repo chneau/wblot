63c6bf5800a365d091e6d2fdd98d5ca64b94d401
chneau <charles63500@gmail.com>
2023-02-17 10:04:18 +0000

    drag n drop file

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import Konva from "konva";
 import { useState } from "react";
-import { Layer, Rect, Stage, Text } from "react-konva";
+import { useDropzone } from "react-dropzone";
+import { Image as KonvaImage, Layer, Rect, Stage, Text } from "react-konva";
 
 const ColoredRect = () => {
   const [color, setColor] = useState(Konva.Util.getRandomColor());
-  const handleClick = () => setColor(Konva.Util.getRandomColor());
-  return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={handleClick} />;
+  return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={() => setColor(Konva.Util.getRandomColor())} />;
 };
 
 export const App = () => {
+  const [image, setImage] = useState<HTMLImageElement | undefined>(undefined);
+  const onDrop = (files: File[]) => {
+    const image = new Image();
+    image.src = URL.createObjectURL(files[0]);
+    image.onload = () => setImage(image);
+  };
+  const { getRootProps } = useDropzone({ onDrop, noClick: true });
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text="Try click on rect" />
-        <ColoredRect />
-      </Layer>
-    </Stage>
+    <div {...getRootProps()}>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>{image ? <KonvaImage image={image} /> : <Text text="Drag and drop a picture" />}</Layer>
+      </Stage>
+    </div>
   );
 };
