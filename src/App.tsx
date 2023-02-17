7aa3b43a2b364a4eaee22e032e124c74fd668ab8
chneau <charles63500@gmail.com>
2023-02-17 12:12:24 +0000

    oki

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,60 @@
 import Konva from "konva";
 import { useState } from "react";
 import { useDropzone } from "react-dropzone";
-import { Image as KonvaImage, Layer, Rect, Stage, Text } from "react-konva";
+import { Image as KonvaImage, Layer, Stage, Text } from "react-konva";
+import { BorderRectWithText } from "./BorderRectWithText";
 
-const ColoredRect = () => {
-  const [color, setColor] = useState(Konva.Util.getRandomColor());
-  return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={() => setColor(Konva.Util.getRandomColor())} />;
-};
-
-interface BorderRectWithText {
+interface Rectangles {
   value: number;
   x: number;
   y: number;
-  width: number;
-  height: number;
 }
-const BorderRectWithText = ({ value, x, y, width, height }: BorderRectWithText) => {
-  const [xPos, setXPos] = useState(x);
-  const [yPos, setYPos] = useState(y);
-  const onDragMove = (evt: Konva.KonvaEventObject<DragEvent>) => {
-    setXPos(evt.target.x());
-    setYPos(evt.target.y());
-  };
-  return (
-    <Layer x={xPos} y={yPos} onDragMove={onDragMove} draggable>
-      <Text text={`${value}`} />
-      <Rect width={width} height={height} stroke="red" strokeWidth={1} />
-    </Layer>
-  );
-};
-
 export const App = () => {
   const [image, setImage] = useState<HTMLImageElement | undefined>(undefined);
+  const [width, setWidth] = useState(45);
+  const [height, setHeight] = useState(15);
+  const [rectangles, setRectangles] = useState<Rectangles[]>([]);
   const onDrop = (files: File[]) => {
     const image = new Image();
     image.src = URL.createObjectURL(files[0]);
     image.onload = () => setImage(image);
   };
   const { getRootProps } = useDropzone({ onDrop, noClick: true });
+  const onDragMoveHandler = (idx: number, evt: Konva.KonvaEventObject<DragEvent>) => {
+    const newRectangles = [...rectangles];
+    newRectangles[idx] = { ...newRectangles[idx], x: evt.target.x(), y: evt.target.y() };
+    setRectangles(newRectangles);
+  };
+  const onCanvasClick = (evt: Konva.KonvaEventObject<MouseEvent>) => {
+    if (!evt.evt.ctrlKey) return;
+    const stage = evt.target.getStage();
+    if (!stage) return console.error("No stage");
+    const pointerPosition = stage.getPointerPosition();
+    if (!pointerPosition) return console.error("No pointer position");
+    let { x, y } = pointerPosition;
+    x -= width / 2;
+    y -= height / 2;
+    setRectangles([...rectangles, { value: rectangles.length + 1, x, y }]);
+  };
+  const onClickHandler = (idx: number, evt: Konva.KonvaEventObject<MouseEvent>) => {
+    if (!evt.evt.ctrlKey) return;
+    const newRectangles = rectangles.filter((_, i) => idx != i);
+    setRectangles(newRectangles);
+  };
   return (
     <div {...getRootProps()}>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Stage width={window.innerWidth} height={window.innerHeight} onClick={onCanvasClick}>
         <Layer>{image ? <KonvaImage image={image} /> : <Text text="Drag and drop a picture" x={window.innerWidth / 2} y={window.innerHeight / 2} />}</Layer>
-        <BorderRectWithText value={550} x={50} y={50} width={45} height={15} />
-        {/* TODO: How to keep track of the state of multiple of them? */}
-        {/* as well, it needs a onDragMove callback to recalculate the value of the Rect from the image */}
+        {rectangles.map((rect, idx) => (
+          <BorderRectWithText //
+            key={idx}
+            width={width}
+            height={height}
+            onDragMove={(evt) => onDragMoveHandler(idx, evt)}
+            onClick={(evt) => onClickHandler(idx, evt)}
+            {...rect}
+          />
+        ))}
       </Stage>
     </div>
   );
---
+++ src/BorderRectWithText.tsx
@@ -0,0 +1,18 @@
+import Konva from "konva";
+import { Layer, Rect, Text } from "react-konva";
+
+interface BorderRectWithText {
+  value: number;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  onDragMove: (evt: Konva.KonvaEventObject<DragEvent>) => void;
+  onClick: (evt: Konva.KonvaEventObject<MouseEvent>) => void;
+}
+export const BorderRectWithText = ({ value, x, y, width, height, onDragMove, onClick }: BorderRectWithText) => (
+  <Layer x={x} y={y} onDragMove={onDragMove} onClick={onClick} draggable>
+    <Text text={`${value}`} stroke="blue" strokeWidth={1} />
+    <Rect width={width} height={height} stroke="red" strokeWidth={1} />
+  </Layer>
+);
