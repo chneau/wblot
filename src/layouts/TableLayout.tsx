0c05d7531cee46b515b751044dc92bab1bff347e
chneau <charles63500@gmail.com>
2023-02-20 11:56:35 +0000

    ok

--- src/layouts/TableLayout.tsx
+++ src/layouts/TableLayout.tsx
@@ -1,3 +1,19 @@
-export const Table = () => {
-  return <div>Table</div>;
+import { Table } from "antd";
+import { useContext } from "react";
+import { BlotContext } from "../contexts/BlotContext";
+
+export const TableLayout = () => {
+  const { rectangles } = useContext(BlotContext);
+  const sortedRectangles = [...rectangles].sort((a, b) => a.x - b.x);
+  const values = [sortedRectangles.reduce((acc, cur, idx) => ({ ...acc, [idx]: cur.value }), {})];
+  return (
+    <div>
+      <Table //
+        size="small"
+        pagination={false}
+        dataSource={values}
+        columns={sortedRectangles.map((_, idx) => ({ dataIndex: idx, title: idx }))}
+      />
+    </div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BlotProvider } from "./contexts/BlotProvider";
 import { CanvasLayout } from "./layouts/CanvasLayout";
-import { Table } from "./layouts/TableLayout";
+import { TableLayout } from "./layouts/TableLayout";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -16,7 +16,7 @@ createRoot(document.getElementById("root")!).render(
               <CanvasLayout />
             </Col>
             <Col span={6}>
-              <Table />
+              <TableLayout />
             </Col>
           </Row>
         </Layout.Content>
