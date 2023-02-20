6ec20b9895393ebaaad2e60eb35bef1f707f5784
chneau <charles63500@gmail.com>
2023-02-20 12:08:06 +0000

    fix

--- src/layouts/TableLayout.tsx
+++ src/layouts/TableLayout.tsx
@@ -5,15 +5,7 @@ import { BlotContext } from "../contexts/BlotContext";
 export const TableLayout = () => {
   const { rectangles } = useContext(BlotContext);
   const sortedRectangles = [...rectangles].sort((a, b) => a.x - b.x);
-  const values = [sortedRectangles.reduce((acc, cur, idx) => ({ ...acc, [idx]: cur.value }), {})];
-  return (
-    <div>
-      <Table //
-        size="small"
-        pagination={false}
-        dataSource={values}
-        columns={sortedRectangles.map((_, idx) => ({ dataIndex: idx, title: idx }))}
-      />
-    </div>
-  );
+  const values = [sortedRectangles.reduce((acc, cur, idx) => ({ ...acc, [idx]: cur.value }), { key: 0 })];
+  const columns = sortedRectangles.map((_, idx) => ({ dataIndex: idx, title: idx + 1 }));
+  return <div>{rectangles.length == 0 ? <>No data</> : <Table size="small" pagination={false} dataSource={values} columns={columns} />}</div>;
 };
